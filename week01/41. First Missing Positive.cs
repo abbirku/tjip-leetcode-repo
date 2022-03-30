5b68d050e5e3b0752e0f6be21f2597aff9891903
abbirku <abbir.ku@gmail.com>
2022-03-31 03:38:05 +0600

    update First missing positive

--- week01/41. First Missing Positive.cs
+++ week01/41. First Missing Positive.cs
@@ -1,15 +1,51 @@
 public class Solution {
     public int FirstMissingPositive(int[] nums)
     {
-        double maxNum = nums.Max();
-        var hashSet = new HashSet<int>(nums);
-        for (int i = 1; i <= maxNum + 1; i++)
+        var heightNum = 0;
+        var containsOne = false;
+
+        nums = nums.Where(x => x != 0).ToArray();
+
+        for (int i = 0; i < nums.Length; i++)
         {
-            if (!hashSet.Contains(i))
+            if (nums[i] == 1)
+                containsOne = true;
+
+            if (nums[i] < 0)
+                nums[i] = 1;
+
+            if (heightNum < nums[i])
+                heightNum = nums[i];
+        }
+
+        if (!containsOne)
+            return 1;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var chosen = nums[i];
+            chosen = chosen < 0 ? chosen * (-1) : chosen;
+
+            if (chosen <= nums.Length)
             {
-                return i;
+                if (chosen > 0 && nums[chosen - 1] > 0)
+                    nums[chosen - 1] *= -1;
             }
         }
-        return 0;
+
+        var allNegative = nums.All(x => x < 0);
+
+        if (allNegative)
+            return heightNum + 1;
+        else
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                    return i + 1;
+            }
+        }
+
+        return 1;
     }
 }
