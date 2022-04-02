891b1214d7b033f68f7f061c72e8409f98ebb356
abbirku <abbir.ku@gmail.com>
2022-04-03 00:28:28 +0600

    Improvide 3Sum Code

--- week01/15. 3Sum.cs
+++ week01/15. 3Sum.cs
@@ -1,70 +1,61 @@
 public class Solution {
-    public List<int> Sort(List<int> list)
+    public int GetSequenceHashCode(IList<int> sequence)
+    {
+        int hash = 0;
+
+        foreach (int integer in sequence)
         {
-            if(list[1] < list[0])
-            {
-                var temp = list[1];
-                list[1] = list[0];
-                list[0] = temp;
-            }
+            int x = integer;
 
-            if (list[2] < list[1])
-            {
-                var temp = list[2];
-                list[2] = list[1];
-                list[1] = temp;
-            }
+            x ^= x >> 17;
+            x *= 830770091;   // 0xed5ad4bb
+            x ^= x >> 11;
+            x *= -1404298415; // 0xac4c1b51
+            x ^= x >> 15;
+            x *= 830770091;   // 0x31848bab
+            x ^= x >> 14;
 
-            if (list[1] < list[0])
-            {
-                var temp = list[1];
-                list[1] = list[0];
-                list[0] = temp;
-            }
-
-            return list;
+            hash += x;
         }
 
-        public List<IList<int>> TwoSum(int[] nums, int index, int target, List<IList<int>> finalRes)
-        {
-            var hash = new Dictionary<int, int>();
+        return hash;
+    }
+    
+    public void TwoSum(int[] nums, int index, int target, Dictionary<int, IList<int>> finalRes)
+    {
+        var keyValSet = new Dictionary<int, int>();
 
-            for (int i = 0; i < nums.Length; i++)
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i != index)
             {
-                if(i != index)
+                var y = target - nums[i];
+                if (keyValSet.ContainsKey(y))
                 {
-                    var y = target - nums[i];
-                    if (hash.ContainsKey(y))
-                    {
-                        var list = new List<int> { nums[i], hash[y], nums[index] };
-                        list = Sort(list);
+                    var list = new List<int> { nums[i], keyValSet[y], nums[index] };
 
-                        if (finalRes.Where(x => x.SequenceEqual(list)).ToList().Count == 0)
-                            finalRes.Add(list);
+                    var listHashCode = GetSequenceHashCode(list);
+                    if(!finalRes.ContainsKey(listHashCode))
+                        finalRes.Add(listHashCode, list);
 
-                        hash.Remove(y);
-                    }
-                    else
-                    {
-                        if(!hash.ContainsKey(nums[i]))
-                            hash.Add(nums[i], nums[i]);
-                    }
+                    keyValSet.Remove(y);
                 }
+                else if (!keyValSet.ContainsKey(nums[i]))
+                    keyValSet.Add(nums[i], nums[i]);
             }
-
-            return finalRes;
         }
+    }
 
-        public IList<IList<int>> ThreeSum(int[] nums)
-        {
-            if (nums == null || (nums != null && nums.Length < 3))
-                return new List<IList<int>>();
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        if (nums == null || (nums != null && nums.Length < 3))
+            return new List<IList<int>>();
 
-            var finalRes = new List<IList<int>>();
+        var finalRes = new Dictionary<int, IList<int>>();
 
-            for (int i = 0; i < nums.Length; i++)
-                TwoSum(nums, i, nums[i] * (-1), finalRes);
+        for (int i = 0; i < nums.Length; i++)
+            TwoSum(nums, i, nums[i] * (-1), finalRes);
 
-            return finalRes;
-        }
+        return finalRes.Select(x=>x.Value).ToList();
+    }
 }
